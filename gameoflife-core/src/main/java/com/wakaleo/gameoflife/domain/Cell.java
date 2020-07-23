ea76bd14b7c6b391697571b8074e7f1b0892acd6
Tsiskomichelis Stelios <=>
2020-07-23 13:08:39 +0300

    Test commit

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -6,6 +6,8 @@ package com.wakaleo.gameoflife.domain;
  *
  * Cell.java
  * Cell object class, contains information on individual cells (positions) in the grid
+ * 
+ * testing
  */
 public enum Cell {
 	// Symbols to represent cell status
